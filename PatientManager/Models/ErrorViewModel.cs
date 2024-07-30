ce747b37a0869e35bc588cce7fe637fb29125183
Enrique Nina <enriquenina06@hotmail.com>
2024-07-30 08:54:51 -0400

    Add existing files

---
+++ PatientManager/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PatientManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
