ddb32e9c0626dd1cafeea0cff84d3ef37d040975
Enrique Nina <enriquenina06@hotmail.com>
2024-07-30 18:25:57 -0400

    Add Doctor Entity

---
+++ Core/PatientMgmt.Core.Domain/Entities/Doctor.cs
@@ -0,0 +1,12 @@
+﻿namespace PatientMgmt.Core.Domain;
+
+public class Doctor:AuditableBaseEntity
+{
+  public string Name { get; set; }  
+  public string LastName { get; set; }
+  public string Email { get; set; }
+  public int Phone { get; set; }
+  public int IdCard { get; set; }
+  public string? Picture { get; set; }
+
+}
