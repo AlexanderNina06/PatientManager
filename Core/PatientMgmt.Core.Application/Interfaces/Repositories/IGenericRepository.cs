aed1766ec54584ee51f189890da7b3a0af2126d4
Enrique Nina <enriquenina06@hotmail.com>
2024-08-01 08:45:48 -0400

    modified IGeenericRepository methods

--- Core/PatientMgmt.Core.Application/Interfaces/Repositories/IGenericRepository.cs
+++ Core/PatientMgmt.Core.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -3,8 +3,8 @@
 public interface IGenericRepository<Entity> where Entity : class
 {
 Task<Entity> AddAsync(Entity entity);
-Task<Entity> UpdateAsync(Entity entity, int id);
-Task<Entity> DeleteAsync(Entity entity);
+Task UpdateAsync(Entity entity, int id);
+Task DeleteAsync(Entity entity);
 Task<List<Entity>> GetAllAsync();
 Task<Entity> GetByIdAsync(int id);
 
