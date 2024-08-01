e4d998f09a40465447e07d58f6de16b8365c61f4
Enrique Nina <enriquenina06@hotmail.com>
2024-08-01 08:46:27 -0400

    Add GenericRepository

---
+++ Infrastructure/PatientMgmt.Infrastructure.Persistence/Repository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using PatientMgmt.Core.Application;
+
+namespace PatientMgmt.Infrastructure.Persistence;
+
+public class GenericRepository<Entity> : IGenericRepository<Entity> where Entity : class
+{
+  private readonly ApplicationContext _db;
+  public GenericRepository(ApplicationContext context){
+
+      _db = context;
+  
+  }
+    public virtual async Task<Entity> AddAsync(Entity entity)
+    {
+        await _db.Set<Entity>().AddAsync(entity);
+        await _db.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task DeleteAsync(Entity entity)
+    {
+        _db.Set<Entity>().Remove(entity);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<List<Entity>> GetAllAsync()
+    {
+        return await _db.Set<Entity>().ToListAsync();
+    }
+
+    public async Task<Entity> GetByIdAsync(int id)
+    {
+        return await _db.Set<Entity>().FindAsync(id);
+    }
+
+    public async Task UpdateAsync(Entity entity, int id)
+    {
+        Entity entry = await _db.Set<Entity>().FindAsync(id);
+        _db.Entry(entry).CurrentValues.SetValues(entity);
+        await _db.SaveChangesAsync();
+    }
+}
