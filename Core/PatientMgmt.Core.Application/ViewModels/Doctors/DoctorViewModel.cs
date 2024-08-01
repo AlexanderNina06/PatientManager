789b358cdb32a588fba7763274dffb4ac6bdb308
Enrique Nina <enriquenina06@hotmail.com>
2024-08-01 10:55:53 -0400

    Add Doctor ViewModels

---
+++ Core/PatientMgmt.Core.Application/ViewModels/Doctors/DoctorViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PatientMgmt.Core.Application;
+
+public class DoctorViewModel
+{
+public int id { get; set; }
+public string Name { get; set; }  
+public string LastName { get; set; }
+public string Email { get; set; }
+public int Phone { get; set; }
+public int IdCard { get; set; }
+public string? Picture { get; set; }
+}
