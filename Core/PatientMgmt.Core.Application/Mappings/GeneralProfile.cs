72be501c19b34bd00ce09c1aaf860a21f6ea8ebb
Enrique Nina <enriquenina06@hotmail.com>
2024-08-01 10:55:19 -0400

    Add Maping File

--- Core/PatientMgmt.Core.Application/Mappings/GeneralProfile.cs
+++ Core/PatientMgmt.Core.Application/Mappings/GeneralProfile.cs
@@ -1,6 +1,29 @@
-﻿namespace PatientMgmt.Core.Application;
+﻿using AutoMapper;
+using PatientMgmt.Core.Domain;
 
-public class GeneralProfile
+namespace PatientMgmt.Core.Application;
+
+public class GeneralProfile : Profile
+{
+public GeneralProfile()
 {
 
+// Product
+CreateMap<Doctor,DoctorViewModel>()
+.ReverseMap()
+.ForMember(dest => dest.Created, opt => opt.Ignore())
+.ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+.ForMember(dest => dest.LastModified, opt => opt.Ignore())
+.ForMember(dest => dest.LastModifiedBy, opt => opt.Ignore());
+
+CreateMap<Doctor,SaveDoctorViewModel>()
+.ForMember(dest => dest.PictureFile, opt => opt.Ignore())
+.ReverseMap()
+.ForMember(dest => dest.Created, opt => opt.Ignore())
+.ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+.ForMember(dest => dest.LastModified, opt => opt.Ignore())
+.ForMember(dest => dest.LastModifiedBy, opt => opt.Ignore());
+
+
+}
 }
