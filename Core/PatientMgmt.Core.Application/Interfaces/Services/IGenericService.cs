e50b63375a37208e2edf6a8550c1d779db62527c
Enrique Nina <enriquenina06@hotmail.com>
2024-08-01 08:57:56 -0400

    Add interface IGenericService

---
+++ Core/PatientMgmt.Core.Application/Interfaces/Services/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿namespace PatientMgmt.Core.Application;
+
+public interface IGenericService<SaveViewModel, ViewModel, Entity> 
+where SaveViewModel : class
+where ViewModel : class
+where Entity : class
+{
+
+Task<SaveViewModel> Add(SaveViewModel vm);
+Task Update(SaveViewModel vm, int id);
+Task Delete(int id);
+Task<List<ViewModel>> GetAll();
+Task<SaveViewModel> GetByIdSaveViewModel(int id);
+
+}
