0831e9c378b0dd7354bc1e8aa8e90e349ed86942
Enrique Nina <enriquenina06@hotmail.com>
2024-08-01 18:32:26 -0400

    Modify General Profile

--- Core/PatientMgmt.Core.Application/Mappings/GeneralProfile.cs
+++ Core/PatientMgmt.Core.Application/Mappings/GeneralProfile.cs
@@ -8,7 +8,7 @@ public class GeneralProfile : Profile
 public GeneralProfile()
 {
 
-// Product
+// Product LabTest Mappings
 CreateMap<Doctor,DoctorViewModel>()
 .ReverseMap()
 .ForMember(dest => dest.Created, opt => opt.Ignore())
@@ -24,6 +24,66 @@ CreateMap<Doctor,SaveDoctorViewModel>()
 .ForMember(dest => dest.LastModified, opt => opt.Ignore())
 .ForMember(dest => dest.LastModifiedBy, opt => opt.Ignore());
 
+// Patient LabTest Mappings
+CreateMap<Patient,PatientViewModel>()
+.ReverseMap()
+.ForMember(dest => dest.Created, opt => opt.Ignore())
+.ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+.ForMember(dest => dest.LastModified, opt => opt.Ignore())
+.ForMember(dest => dest.LastModifiedBy, opt => opt.Ignore());
+
+CreateMap<Patient,SavePatientViewModel>()
+.ForMember(dest => dest.PictureFile, opt => opt.Ignore())
+.ReverseMap()
+.ForMember(dest => dest.Created, opt => opt.Ignore())
+.ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+.ForMember(dest => dest.LastModified, opt => opt.Ignore())
+.ForMember(dest => dest.LastModifiedBy, opt => opt.Ignore());
+
+// LabTest Mappings
+CreateMap<LabTest,LabTestViewModel>()
+.ReverseMap()
+.ForMember(dest => dest.Created, opt => opt.Ignore())
+.ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+.ForMember(dest => dest.LastModified, opt => opt.Ignore())
+.ForMember(dest => dest.LastModifiedBy, opt => opt.Ignore());
+
+CreateMap<LabTest,SaveLabTestViewModel>()
+.ReverseMap()
+.ForMember(dest => dest.Created, opt => opt.Ignore())
+.ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+.ForMember(dest => dest.LastModified, opt => opt.Ignore())
+.ForMember(dest => dest.LastModifiedBy, opt => opt.Ignore());
+
+// Appointment Mappings
+CreateMap<Appointment,AppointmentViewModel>()
+.ForMember(dest => dest.PatientName, opt => opt.MapFrom(src => $"{src.patient.Name} {src.patient.LastName}"))
+.ForMember(dest => dest.DoctorName, opt => opt.MapFrom(src => $"{src.doctor.Name} {src.doctor.LastName}"))
+.ForMember(dest => dest.appointmentStatus, opt => opt.MapFrom(src => src.appointmentStatus.ToString()))
+.ForMember(dest => dest.TestFinalResults, opt => opt.Ignore())
+.ForMember(dest => dest.TestResultState, opt => opt.Ignore())
+.ForMember(dest => dest.TestsNames, opt => opt.Ignore())
+.ReverseMap()
+.ForMember(dest => dest.Created, opt => opt.Ignore())
+.ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+.ForMember(dest => dest.LastModified, opt => opt.Ignore())
+.ForMember(dest => dest.LastModifiedBy, opt => opt.Ignore())
+.ForMember(dest => dest.doctor, opt => opt.Ignore())
+.ForMember(dest => dest.tests, opt => opt.Ignore())
+.ForMember(dest => dest.TestResults, opt => opt.Ignore())
+.ForMember(dest => dest.patient, opt => opt.Ignore());
+
+// TestResults Mappings
+CreateMap<TestResult,TestResultsViewModel>()
+.ForMember(dest => dest.PatientName, opt => opt.MapFrom(src => $"{src.patient.Name} {src.patient.LastName}"))
+.ForMember(dest => dest.ResultStatus, opt => opt.MapFrom(src => src.ResultStatus.ToString()))
+.ReverseMap()
+.ForMember(dest => dest.Created, opt => opt.Ignore())
+.ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+.ForMember(dest => dest.LastModified, opt => opt.Ignore())
+.ForMember(dest => dest.LastModifiedBy, opt => opt.Ignore())
+.ForMember(dest => dest.Appointment, opt => opt.Ignore())
+.ForMember(dest => dest.patient, opt => opt.Ignore());
 
 }
 }
