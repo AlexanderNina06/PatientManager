6b4ec839e969dae0fd2c0d620c3280a643ac94d2
Enrique Nina <enriquenina06@hotmail.com>
2024-08-01 18:46:10 -0400

    Add user view models

---
+++ Core/PatientMgmt.Core.Application/ViewModels/Users/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatientMgmt.Core.Application;
+
+public class LoginViewModel
+{
+[Required(ErrorMessage = "Username is required")]
+[DataType(DataType.Text)]
+public string UserName { get; set; }
+
+[Required(ErrorMessage = "Password is required")]
+[DataType(DataType.Password)]
+public string Password { get; set; }
+public bool HasError { get; set; }
+public string? Error { get; set; }
+}
---
+++ Core/PatientMgmt.Core.Application/ViewModels/Users/SaveUserViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatientMgmt.Core.Application;
+
+public class SaveUserViewModel
+{
+[Required(ErrorMessage = "Name field is required")]
+[DataType(DataType.Text)]
+public string FirstName { get; set; }
+
+[Required(ErrorMessage = "Last name field is required")]
+[DataType(DataType.Text)]
+public string LastName { get; set; }
+
+[Required(ErrorMessage = "Username field is required")]
+[DataType(DataType.Text)]
+public string UserName { get; set; }
+
+[Required(ErrorMessage = "Password is required")]
+[DataType(DataType.Password)]
+public string Password { get; set; }
+
+[Compare(nameof(Password),ErrorMessage ="The passwords are not the same")]
+[Required(ErrorMessage = "Password is required")]
+[DataType(DataType.Password)]
+public string ConfirmPassword { get; set; }
+
+[Required(ErrorMessage = "Email field is required")]
+[DataType(DataType.EmailAddress)]
+public string Email { get; set; }
+
+[Required(ErrorMessage = "Phone field is required")]
+[DataType(DataType.PhoneNumber)]
+public string Phone { get; set; }
+
+public bool HasError { get; set; }
+public string? Error { get; set; }
+}
