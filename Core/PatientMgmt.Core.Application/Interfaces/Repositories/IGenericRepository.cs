7345410c1d64e38ff860c31fafdc3ba7b0b4afef
Enrique Nina <enriquenina06@hotmail.com>
2024-08-01 07:40:46 -0400

    Add Interface Folder

---
+++ Core/PatientMgmt.Core.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace PatientMgmt.Core.Application;
+
+public interface IGenericRepository<Entity> where Entity : class
+{
+Task<Entity> AddAsync(Entity entity);
+Task<Entity> UpdateAsync(Entity entity, int id);
+Task<Entity> DeleteAsync(Entity entity);
+Task<List<Entity>> GetAllAsync();
+Task<Entity> GetByIdAsync(int id);
+
+}
