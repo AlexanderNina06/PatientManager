360a5bd4b0a018dd35f9562dc0e4bbdf8bfafcc8
Enrique Nina <enriquenina06@hotmail.com>
2024-08-05 12:17:57 -0400

    Add interface IUserService

---
+++ Core/PatientMgmt.Core.Application/Interfaces/Services/IUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace PatientMgmt.Core.Application;
+
+public interface IUserService
+{
+Task<AuthenticationResponse> LoginAsync(LoginViewModel vm);
+Task<RegisterResponse> RegisterAsync(SaveUserViewModel vm, string origin);
+Task SignOutAsync();
+}
