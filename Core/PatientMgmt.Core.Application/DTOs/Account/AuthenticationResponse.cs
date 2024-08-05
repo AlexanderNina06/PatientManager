a189e7ca62cca4e784eeb4aeb6ef2a25cd787d35
Enrique Nina <enriquenina06@hotmail.com>
2024-08-05 12:11:37 -0400

    Modify AuthenticationResponse file

--- Core/PatientMgmt.Core.Application/DTOs/Account/AuthenticationResponse.cs
+++ Core/PatientMgmt.Core.Application/DTOs/Account/AuthenticationResponse.cs
@@ -2,6 +2,10 @@
 
 public class AuthenticationResponse
 {
+public string Id { get; set; }
 public string UserName { get; set; }
-public string Password { get; set; }
+public string Email { get; set; }
+public List<string> Roles { get; set; }
+public bool HasError { get; set; }
+public string Error { get; set; }
 }
