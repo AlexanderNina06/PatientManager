dc9cc306d171dac26f005ec460c2c62706018e24
Enrique Nina <enriquenina06@hotmail.com>
2024-08-05 12:16:20 -0400

    Add interface IAccountService

---
+++ Core/PatientMgmt.Core.Application/DTOs/Account/AuthenticationRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace PatientMgmt.Core.Application;
+
+public class AuthenticationRequest
+{
+public string UserName { get; set; }
+public string Password { get; set; }
+}
---
+++ Core/PatientMgmt.Core.Application/Interfaces/Services/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿namespace PatientMgmt.Core.Application;
+
+public interface IAccountService
+{
+Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request);
+Task<RegisterResponse> RegisterAdminUserAsync(RegisterRequest request, string origin);
+Task SignOutAsync();
+Task<string> ConfirmAcountAsync(string userId, string token);
+
+}
