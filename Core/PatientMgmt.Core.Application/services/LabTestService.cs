3b43a021eed05930dd3df3d2d6063ae0bdc27af9
Enrique Nina <enriquenina06@hotmail.com>
2024-08-05 17:28:55 -0400

    LabTest Service

---
+++ Core/PatientMgmt.Core.Application/services/LabTestService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using PatientMgmt.Core.Domain;
+
+namespace PatientMgmt.Core.Application;
+
+public class LabTestService : GenericService<SaveLabTestViewModel, LabTestViewModel, LabTest>
+{
+  private readonly ILabTestRepository _labTestRepository;
+  private readonly IHttpContextAccessor _httpContextAccessor;
+  private readonly AuthenticationResponse userViewModel;
+  private readonly IMapper _mapper;
+    public LabTestService(ILabTestRepository labTestRepository, IHttpContextAccessor httpContextAccessor, IMapper mapper) : base(labTestRepository, mapper)
+    {
+      _labTestRepository = labTestRepository;
+      _httpContextAccessor = httpContextAccessor;
+      _mapper = mapper;
+    }
+}
