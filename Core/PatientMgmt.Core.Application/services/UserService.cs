f6563684490f848bb272bfd7f8b1fefb033eda3f
Enrique Nina <enriquenina06@hotmail.com>
2024-08-05 12:35:03 -0400

    Implement methods in User Service

--- Core/PatientMgmt.Core.Application/services/UserService.cs
+++ Core/PatientMgmt.Core.Application/services/UserService.cs
@@ -7,22 +7,26 @@ public class UserService : IUserService
 {
   private readonly IAccountService _accountService;
   private readonly IMapper _mapper;
-  public UserService()
+  public UserService(IAccountService accountService, IMapper mapper)
   {
-
+    _accountService = accountService;
+    _mapper = mapper;
   }
-    public Task<AuthenticationResponse> LoginAsync(LoginViewModel vm)
+    public async Task<AuthenticationResponse> LoginAsync(LoginViewModel vm)
     {
-        throw new NotImplementedException();
+        AuthenticationRequest loginRequest = _mapper.Map<AuthenticationRequest>(vm);
+        AuthenticationResponse userResponse = await _accountService.AuthenticateAsync(loginRequest);
+        return userResponse;
     }
 
-    public Task<RegisterResponse> RegisterAsync(SaveUserViewModel vm, string origin)
+    public async Task<RegisterResponse> RegisterAsync(SaveUserViewModel vm, string origin)
     {
-        throw new NotImplementedException();
+        RegisterRequest registerRequest= _mapper.Map<RegisterRequest>(vm);
+        return await _accountService.RegisterAdminUserAsync(registerRequest, origin);
     }
 
-    public Task SignOutAsync()
+    public async Task SignOutAsync()
     {
-        throw new NotImplementedException();
+        await _accountService.SignOutAsync();
     }
 }
