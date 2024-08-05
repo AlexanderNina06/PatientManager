ffc73264d549a89c700b5d578801b3d400cb7057
Enrique Nina <enriquenina06@hotmail.com>
2024-08-05 12:20:52 -0400

    Add User Service

---
+++ Core/PatientMgmt.Core.Application/services/UserService.cs
@@ -0,0 +1,28 @@
+﻿
+using AutoMapper;
+
+namespace PatientMgmt.Core.Application;
+
+public class UserService : IUserService
+{
+  private readonly IAccountService _accountService;
+  private readonly IMapper _mapper;
+  public UserService()
+  {
+
+  }
+    public Task<AuthenticationResponse> LoginAsync(LoginViewModel vm)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RegisterResponse> RegisterAsync(SaveUserViewModel vm, string origin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SignOutAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
