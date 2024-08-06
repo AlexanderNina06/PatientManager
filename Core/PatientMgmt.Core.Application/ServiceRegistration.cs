527593e8eda46f4c5d5844d314e2614d8a8ae286
Enrique Nina <enriquenina06@hotmail.com>
2024-08-06 15:04:41 -0400

    Add  Services in Application Service Registration

---
+++ Core/PatientMgmt.Core.Application/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PatientMgmt.Core.Domain;
+
+namespace PatientMgmt.Core.Application;
+
+public static class ServiceRegistration
+{
+public static void AddApplicationLayer(this IServiceCollection services, IConfiguration configuration)
+{
+	services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+	#region Services
+	  services.AddTransient<IDoctorService, DoctorService>();
+    services.AddTransient<IPatientService, PatientService>();
+    services.AddTransient<ILabTestService, LabTestService>();
+    services.AddTransient<IUserService, UserService>();
+    #endregion
+}
+}
