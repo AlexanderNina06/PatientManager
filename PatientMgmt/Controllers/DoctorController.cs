44c54e72e18bc28c65277743ec99b99470512b09
Enrique Nina <enriquenina06@hotmail.com>
2024-08-07 09:40:29 -0400

    Add Doctor Controller

---
+++ PatientMgmt/Controllers/DoctorController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PatientMgmt.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: DoctorController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
