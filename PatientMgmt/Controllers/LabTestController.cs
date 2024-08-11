c7ee10068e682d207459f07f8378ba04ff5e8e8e
Enrique Nina <enriquenina06@hotmail.com>
2024-08-11 18:30:42 -0400

    Add Lab Test Controller

---
+++ PatientMgmt/Controllers/LabTestController.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using PatientMgmt.Core.Application;
+
+namespace PatientMgmt.Controllers
+{
+    public class LabTestController : Controller
+    {
+        private readonly ILabTestService _labTestService;
+        public LabTestController(ILabTestService labTestService){
+            _labTestService = labTestService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _labTestService.GetAll());
+        }
+
+        public IActionResult Create()
+        {  
+            return View("SaveLabTest", new SaveLabTestViewModel());
+        }
+        
+        [HttpPost]
+        public async Task <IActionResult> Create(SaveLabTestViewModel vm)
+        {  
+            if(!ModelState.IsValid){
+                return View("SaveLabTest", vm);
+            }
+            
+            await _labTestService.Add(vm);
+            return RedirectToRoute(new {Controller="LabTest", action="Index"});
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View("SaveLabTest", await _labTestService.GetByIdSaveViewModel(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(SaveLabTestViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("SaveLabTest", vm);
+            }
+
+            await _labTestService.Update(vm, vm.Id);
+            return RedirectToRoute(new { controller = "LabTest", action = "Index" });
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(await _labTestService.GetByIdSaveViewModel(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            
+            await _labTestService.Delete(id);
+            return RedirectToRoute(new { controller = "LabTest", action = "Index" });
+        }
+
+    }
+}
