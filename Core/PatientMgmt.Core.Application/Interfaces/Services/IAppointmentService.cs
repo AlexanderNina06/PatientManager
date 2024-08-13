d571dbe57cd4f86e7b7f9f4f1a1302cf90cdd209
Enrique Nina <enriquenina06@hotmail.com>
2024-08-13 11:01:20 -0400

    Add interface IAppointmentService

---
+++ Core/PatientMgmt.Core.Application/Interfaces/Services/IAppointmentService.cs
@@ -0,0 +1,9 @@
+using System;
+using PatientMgmt.Core.Domain;
+
+namespace PatientMgmt.Core.Application.Interfaces.Services;
+
+public interface IAppointmentService : IGenericService<SaveAppointmentViewModel, AppointmentViewModel, Appointment>
+{
+  Task<List<AppointmentViewModel>> GetAllViewModelWithInclude();
+}
