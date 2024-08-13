99a8a2ddfe58e3ac3e2304d3b894c520ae66264b
Enrique Nina <enriquenina06@hotmail.com>
2024-08-13 11:00:41 -0400

    Add interface IAppointmentRepository

---
+++ Core/PatientMgmt.Core.Application/Interfaces/Repositories/IAppointmentRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using PatientMgmt.Core.Domain;
+
+namespace PatientMgmt.Core.Application.Interfaces.Repositories;
+
+public interface IAppointmentRepository : IGenericRepository<Appointment>
+{
+  Task<List<Appointment>> GetAllWithIncludeAsync(List<string> properties);
+
+}
