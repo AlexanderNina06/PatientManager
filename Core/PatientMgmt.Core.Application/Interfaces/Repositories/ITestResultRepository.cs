30ce5a5c1a36202dc3fa79e160b513f8debe9e39
Enrique Nina <enriquenina06@hotmail.com>
2024-08-14 18:14:31 -0400

    Add ItestResult Repository

---
+++ Core/PatientMgmt.Core.Application/Interfaces/Repositories/ITestResultRepository.cs
@@ -0,0 +1,8 @@
+using System;
+using PatientMgmt.Core.Domain;
+
+namespace PatientMgmt.Core.Application.Interfaces.Repositories;
+
+public interface ITestResultRepository : IGenericRepository<TestResult>
+{
+}
